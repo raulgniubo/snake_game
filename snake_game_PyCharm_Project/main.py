f71e0f58fc201a7d46f569f7cbba41303cfba952
Raul Gutierrez Niubo <raulgniubo@gmail.com>
2021-02-03 16:32:09 -0500

    canvas setup done

--- snake_game_PyCharm_Project/main.py
+++ snake_game_PyCharm_Project/main.py
@@ -1,1 +1,28 @@
-print("Hello snake_game")
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")  # the background of the canvas
+screen.title("My Snake Game")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
