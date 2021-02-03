13db14a682c3372e5992298fc702b9fdf397605a
Raul Gutierrez Niubo <raulgniubo@gmail.com>
2021-02-03 16:39:44 -0500

    snake body created

--- snake_game_PyCharm_Project/main.py
+++ snake_game_PyCharm_Project/main.py
@@ -6,8 +6,13 @@ screen.bgcolor("black")  # the background of the canvas
 screen.title("My Snake Game")
 
 
+# Creating the snake body:
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 
 
