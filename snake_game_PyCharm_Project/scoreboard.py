3a534ecaf8d7492ad26b02b960e44a1cc4e8d725
Raul Gutierrez Niubo <raulgniubo@gmail.com>
2021-02-04 16:05:47 -0500

    created some constants for Scoreboard class and changed the font to Courier

--- snake_game_PyCharm_Project/scoreboard.py
+++ snake_game_PyCharm_Project/scoreboard.py
@@ -1,5 +1,7 @@
 
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
 
 class Scoreboard(Turtle):
 
@@ -13,7 +15,7 @@ class Scoreboard(Turtle):
         self.hideturtle()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
