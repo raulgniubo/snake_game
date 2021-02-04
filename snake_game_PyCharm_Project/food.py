6b754b19fd4c699019457c11f7b072b9a871339d
Raul Gutierrez Niubo <raulgniubo@gmail.com>
2021-02-04 18:12:56 -0500

    changed food icon to turtle and appears in random colors

--- snake_game_PyCharm_Project/food.py
+++ snake_game_PyCharm_Project/food.py
@@ -2,8 +2,8 @@
 from turtle import Turtle
 import random
 
-COLOR = "blue"
-SHAPE = "circle"
+COLORS = ["blue", "red", "yellow", "purple"]
+SHAPE = "turtle"
 
 class Food(Turtle):
 
@@ -11,12 +11,17 @@ class Food(Turtle):
         super().__init__()
         self.shape(SHAPE)
         self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.color(COLOR)
+        self.shapesize(stretch_len=0.8, stretch_wid=0.8)
+        self.set_random_color()
         self.speed("fastest")
         self.refresh()
+
+    def set_random_color(self):
+        color = random.choice(COLORS)
+        self.color(color)
 
     def refresh(self):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
+        self.set_random_color()
         self.goto(random_x, random_y)
